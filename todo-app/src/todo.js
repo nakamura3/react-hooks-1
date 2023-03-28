f36b3e66411efe6d433c75f46d97277995077a69
nakamura3 <nakamu@ines.co.jp>
2023-03-29 00:58:37 +0900

    step-6

---
+++ todo-app/src/todo.js
@@ -0,0 +1,35 @@
+const todoDataUrl = "http://localhost:3100/todos";
+
+export const getAllTodosData = async () => {
+  const response = await fetch(todoDataUrl);
+  return response.json();
+};
+
+export const addTodoData = async (todo) => {
+  const response = await fetch(todoDataUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  });
+  return response.json();
+};
+
+export const deleteTodoData = async (id) => {
+  const response = await fetch(`${todoDataUrl}/${id}`, {
+    method: 'DELETE'
+  });
+  return response.json();
+};
+
+export const updateTodoData = async (id, todo) => {
+  const response = await fetch(`${todoDataUrl}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  });
+  return response.json();
+};
